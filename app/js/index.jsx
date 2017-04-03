4733fd5be0d978805626bbf6726ef152b0a2d8c2
Greg Linklater <greglink49@gmail.com>
2017-04-03 15:28:27 +0200

    chore(npm+webpack): initial commit

---
+++ app/js/index.jsx
@@ -0,0 +1,76 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+// import { Provider } from 'react-redux'
+
+// import store from './store'
+// import router from './router.jsx'
+
+/**
+ * Tap Event
+ * @ignore
+ */
+injectTapEventPlugin()
+
+/**
+ * Styles
+ * @ignore
+ */
+const style = {
+  appContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: '100',
+    position: 'relative',
+  }
+}
+
+/**
+ * MainLayout
+ */
+class MainLayout extends React.Component {
+
+  render() {
+    let { appContainer } = style
+
+    return (
+      <div style={appContainer}>
+        Hello World!
+      </div>
+    )
+  }
+
+}
+
+// /**
+//  * DOM
+//  * @ignore
+//  */
+// ReactDOM.render(
+//   <MuiThemeProvider>
+//     <Provider store={store}>
+//       <MainLayout />
+//     </Provider>
+//   </MuiThemeProvider>,
+//   document.getElementById('app')
+// )
+
+/**
+ * DOM
+ * @ignore
+ */
+ReactDOM.render(
+  <MuiThemeProvider>
+    <MainLayout />
+  </MuiThemeProvider>,
+  document.getElementById('app')
+)
