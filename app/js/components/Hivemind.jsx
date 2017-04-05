9e34ebd62558f9187a098cb0483f301b74b36c1f
St John Giddy <stjohngiddy@gmail.com>
2017-04-05 09:15:52 +0200

    wip(hash-router): add a router for in-app navigation

--- app/js/components/Hivemind.jsx
+++ app/js/components/Hivemind.jsx
@@ -8,24 +8,26 @@ import React from 'react'
 import { connect } from 'react-redux'
 import TopNavBar from './TopNavBar.jsx'
 import NavDrawer from './NavDrawer.jsx'
-
+import HivemindRouter from './HivemindRouter.jsx'
 
 const mapDispatchToProps = (dispatch) => {
   return {}
 }
 
 const styles = {
-  navDrawerStyle: {
+  parentContentStyle: {
     position: 'relative',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    flex: 25,
+    alignItems: 'strech',
+    flex: 75,
   },
    contentStyle: {
     position: 'relative',
     display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
     alignItems: 'flex-start',
     flex: 100,
   }
@@ -39,13 +41,13 @@ const mapStateToProps = (state, ownProps) => {
  */
 class Hivemind extends React.Component {
   render(){
-    let { navDrawerStyle, contentStyle } = styles
+    let { parentContentStyle, contentStyle } = styles
     return (
-      <div >
-        <NavDrawer  />        
-          <div >
+      <div style={parentContentStyle}>    
+          <div>
             <TopNavBar style={contentStyle}/>
           </div>
+          {HivemindRouter}
         </div>
 
     )
