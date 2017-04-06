b404597ddb15176edce65b7127fc78373a537b54
St John Giddy <stjohngiddy@gmail.com>
2017-04-06 22:15:48 +0200

    refactor(game): redirect /#/ to /#/game

--- app/js/router.jsx
+++ app/js/router.jsx
@@ -5,7 +5,7 @@
  * @ignore
  */
 import React from 'react'
-import { HashRouter as Router, Route, IndexRoute } from 'react-router-dom'
+import { HashRouter as Router, Route, IndexRoute, Redirect } from 'react-router-dom'
 import createBrowserHistory from 'history/createBrowserHistory'
 import Game from './components/game/Game.jsx'
 import ProfileRoot from './components/profile/ProfileRoot.jsx'
@@ -34,7 +34,7 @@ const styles = {
 export default (
   <Router history={history}>
     <div style={styles.parentContentStyle}>
-      <Route exact={true} path="/" component={Game} />
+      <Route exact={true} path="/" component={() => <Redirect to='/game'/>} />
       <Route path="/game" component={Game} exact={false} />
       <Route path="/Rules" component={() => <h1>Rules</h1>} />
       <Route path="/AdminContact" component={() => <h1>Admin contact</h1>} />
