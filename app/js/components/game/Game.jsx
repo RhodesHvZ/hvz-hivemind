b7edd504c9b0b2f5aff00a579febe0b9195272d6
St John Giddy <stjohngiddy@gmail.com>
2017-04-06 21:37:58 +0200

    wip(game): add filler text

--- app/js/components/game/Game.jsx
+++ app/js/components/game/Game.jsx
@@ -8,7 +8,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import HvZMap from './HvZMap.jsx'
 import Sidebar from './Sidebar.jsx'
-
+import Missions from './Missions.jsx'
 
 /**
  * Styles
@@ -29,11 +29,25 @@ const styles = {
     display: 'flex',
     justifyContent: 'flex-start',
     flexDirection: 'row',
+    paddingLeft: '10px',
+    paddingRight: '10px',
     alignItems: 'flex-start',
     backgroundColor: '#eaeaf2',
     flex: 50,
   }
 }
+const fillerText = (<div>
+                      <h1>Lorem ipsum</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit metus ex, eu laoreet diam sodales eget.
+                          Mauris dapibus lacus vel est gravida, sit amet tincidunt mi vehicula. Sed dapibus orci posuere blandit maximus. <br /> <br />
+                          Mauris vitae velit id quam efficitur imperdiet. Vivamus placerat, arcu cursus sodales vehicula, purus nulla ornare felis,
+                          quis mattis ligula erat eget enim. Aliquam nec congue orci. Donec cursus enim in est iaculis, eu euismod nulla interdum. Donec sed aliquet tortor.
+                          Nullam in finibus ipsum. Nulla eget dui sit amet velit convallis pulvinar at sit amet dolor. Aenean viverra, sem in finibus ultrices,
+                          massa sapien eleifend ipsum, non vehicula massa lorem vitae est. Sed vitae semper leo, pretium rhoncus nulla. Aliquam a turpis venenatis, gravida erat
+                          quis, pretium quam. Donec blandit urna ac ligula sollicitudin, eu condimentum urna aliquam. Praesent nec sapien id erat aliquet tristique. In mollis pellentesque ultrices.
+                      </p>
+                     </div>
+                     )
 
 /**
  * Game
@@ -41,15 +55,15 @@ const styles = {
 class Game extends React.Component {
   render() {
     let { parentContentStyle, contentStyle } = styles
-
+    let filler = fillerText
     return (
       <div style={parentContentStyle}>
         <Sidebar />
         <div style={contentStyle}>
-          <Route path="/" component={() => <h1>Actual Home</h1>} exact={true} />
-          <Route path="/game/" component={() => <h1>Actual Home</h1>} exact={true} />
+          <Route path="/game/" component={() =>  filler } exact={true} />
           <Route path="/game/map" component={HvZMap} />
-          <Route path="/game/byte" component={() => <h1>Byte</h1> } />
+          <Route path="/game/byte" component={() => <h1>ByteCode: {filler} </h1> } />
+          <Route path="/game/missions" component={Missions} />
         </div>
       </div>
     )
