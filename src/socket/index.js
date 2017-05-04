5f40afd8863e66fda349e3274fc4e5477989af0b
Greg Linklater <greglink49@gmail.com>
2017-05-04 07:25:50 +0200

    wip(socket): wip socket manager

--- src/socket/index.js
+++ src/socket/index.js
@@ -4,23 +4,39 @@
  * Dependencies
  * @ignore
  */
-
-
-var io = null
+const socketio = require('socket.io')
+const Events = require('../events')
 
 /**
  * SocketManager
+ *
  * @class
+ * This singleton class handles the socket server.
+ *
+ * @todo inject session info into sockets
  */
 class SocketManager {
-  
-  
-  static get Instance() {
-    return io
-  }
 
-  constructor(socketio) {
-    if(io == null) io = socketio
+  /**
+   * constructor
+   *
+   * @param {HTTPServer} server
+   */
+  constructor (server) {
+    this.io = socketio(server)
+    this.sockets = {}
+
+    this.io.on('connection', function (socket) {
+      console.log('client connected')
+
+      socket.on('message', function (data) {
+        console.log(data)
+      })
+
+      socket.on('PLAYER_TAG', data => {
+        Events.PLAYER_TAG(data)
+      })
+    })
   }
 }
 
@@ -28,4 +44,8 @@ class SocketManager {
  * Export
  * @ignore
  */
-module.exports = SocketManager
+module.exports = (app) => {
+  let instance = new SocketManager(app)
+  module.exports = instance
+  return instance
+}
