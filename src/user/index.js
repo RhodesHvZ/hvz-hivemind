7872b9e0af6168bc45686e223d91617e8c133eb3
Greg Linklater <greglink49@gmail.com>
2017-05-15 13:23:28 +0200

    refactor(UserManager): manager into separate file and linked through index.js

--- src/user/index.js
+++ src/user/index.js
@@ -1,74 +1,10 @@
 'use strict'
 
 /**
- * Dependencies
- * @ignore
- */
-
-/**
  * Module Dependencies
  * @ignore
  */
-const Events = require('../events')
-const SocketManager = require('../socket')
-const SocketEventEnum = require('../../common/SocketEventEnum')
-const Store = require('../common/Store')
-const UserReducer = require('./UserReducer')
-
-/**
- * User Manager
- * @class
- *
- * Maintain user state and handle user events
- */
-class UserManager extends Store {
-
-  /**
-   * constructor
-   */
-  constructor(systemManager) {
-    super()
-    this.systemManager = systemManager
-    //Events.on(Events.eventList.USER_AUTH, event => this.handleUserAuth(event))
-    Events.on(Events.eventList.USER_REGISTERED, event => this.dispatch())
-  }
-
-  /**
-   * reducers
-   */
-  static get reducer () {
-    return UserReducer
-  }
-
-  handleUserAuth (event) {
-    console.log('USER MANAGER', event)
-  }
-
-  handleUserReg(event) {
-
-  }
-
-  registerUser(data, socketid) {
-    let { Status } = SocketEventEnum
-    if (this.state[data.username]) {
-      SocketManager.sockets.to(socketid).emit(Status.Fail, 'Username already exists')
-      return
-    } else {
-      Events.USER_REGISTERED(Object.assign({}, {
-        data: {
-          firstname: data[firstname],
-          lastname: data[lastname],
-          email: data[email],
-          userSettings: {},
-          players: {},
-          achievements: {}
-        },
-        type: 'USER_REGISTERED'
-      }))
-    }
-    // TODO: fire event and reduce
-  }
-}
+const UserManager = require('./UserManager')
 
 /**
  * Export
