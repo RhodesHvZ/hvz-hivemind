d0c0ed383a2a0a7a15da2dbe3ff52cb00ba38151
Greg Linklater <greglink49@gmail.com>
2017-05-15 13:23:46 +0200

    refactor(MissionManager): manager into separate file and linked through index.js

--- src/mission/index.js
+++ src/mission/index.js
@@ -1,27 +1,10 @@
 'use strict'
 
 /**
- * Dependencies
- * @ignore
- */
-
-/**
  * Module Dependencies
  * @ignore
  */
-const Events = require('../events')
-const SocketManager = require('../socket')
-
-/**
- * Mission Manager
- * @class
- */
-class MissionManager {
-
-  constructor () {
-  }
-
-}
+const MissionManager = require('./MissionManager')
 
 /**
  * Export
