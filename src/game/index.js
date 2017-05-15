769acf844318b27d623a0320ba9e03b57e4db9a5
Greg Linklater <greglink49@gmail.com>
2017-05-15 13:23:14 +0200

    refactor(GameManager): manager into separate file and linked through index.js

--- src/game/index.js
+++ src/game/index.js
@@ -1,46 +1,10 @@
 'use strict'
 
 /**
- * Dependencies
- * @ignore
- */
-
-/**
  * Module Dependencies
  * @ignore
  */
-const Store = require('../common/Store')
-const Events = require('../events')
-const SocketManager = require('../socket')
-const GameReducer = require('./GameReducer')
-const PlayerManager = require('../player')
-const SquadManager = require('../squad')
-const MissionManager = require('../mission')
-
-/**
- * Game Manager
- * @class
- */
-class GameManager extends Store {
-
-  /**
-   * constructor
-   */
-  constructor(systemManager) {
-    super()
-    this.systemManager = systemManager
-    this.playerManager = new PlayerManager(this)
-    this.squadManager = new SquadManager(this)
-    this.missionManager = new MissionManager(this)
-  }
-
-  /**
-   * reducers
-   */
-  static get reducer () {
-    return GameReducer
-  }
-}
+const GameManager = require('./GameManager')
 
 /**
  * Export
