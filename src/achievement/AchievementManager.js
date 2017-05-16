fcf0098aa9f9480e83d3cd482a9356586781fdb6
Greg Linklater <greglink49@gmail.com>
2017-05-16 10:47:04 +0200

    wip(AchievementManager): base manager

---
+++ src/achievement/AchievementManager.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+
+/**
+ * Module Dependencies
+ * @ignore
+ */
+const Events = require('../events')
+
+/**
+ * Achievement Manager
+ * @class
+ */
+class AchievementManager {
+
+  constructor (system) {
+    this.system = system
+  }
+
+}
+
+/**
+ * Export
+ * @ignore
+ */
+module.exports = AchievementManager
