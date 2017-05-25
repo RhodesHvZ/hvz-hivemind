3470be829be522e8e8cf2dfcde5582c782feabbe
Greg Linklater <greglink49@gmail.com>
2017-05-25 18:20:48 +0200

    feat(UserManager): user registration and retrieval

---
+++ src/system/managers/user/User.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+
+/**
+ * Module Dependencies
+ * @ignore
+ */
+const Type = require('../../common/Type')
+
+/**
+ * User
+ * @class
+ */
+class User extends Type {
+
+  static get typeName () {
+    return 'user'
+  }
+
+}
+
+/**
+ * Export
+ * @ignore
+ */
+module.exports = User
--- src/system/managers/user/UserManager.js
+++ src/system/managers/user/UserManager.js
@@ -4,6 +4,7 @@
  * Dependencies
  * @ignore
  */
+const moment = require('moment')
 
 /**
  * Module Dependencies
@@ -11,8 +12,9 @@
  */
 const Events = require('../../events')
 const SocketEventEnum = require('../../../../common/SocketEventEnum')
-const Store = require('../../common/Store')
+const Manager = require('../../common/Manager')
 const UserReducer = require('./UserReducer')
+const User = require('./User')
 
 /**
  * User Manager
@@ -20,55 +22,117 @@ const UserReducer = require('./UserReducer')
  *
  * Maintain user state and handle user events
  */
-class UserManager extends Store {
+class UserManager extends Manager {
 
   /**
    * constructor
    */
   constructor (system) {
-    super()
-    this.system = system
-    //Events.on(Events.eventList.USER_AUTH, event => this.handleUserAuth(event))
-    Events.on(Events.eventList.USER_REGISTERED, event => this.dispatch())
+    super(system)
+    Events.on(Events.eventList.USER_AUTH, event => this.handleUserAuth(event))
   }
 
   /**
-   * reducers
+   * type
    */
-  static get reducer () {
-    return UserReducer
+  static get type () {
+    return User
   }
 
+  /**
+   * handleUserAuth
+   *
+   * @description
+   * Handle user authentication
+   *
+   * @param  {Object} event
+   * @return {Promise}
+   */
   handleUserAuth (event) {
-    console.log('USER MANAGER', event)
+    let { userinfo: { sub: id } } = event
+    let { log } = UserManager
+
+    return this.exists({ id }).then(exists => {
+      if (exists) {
+        return this.updateUser(event)
+      } else {
+        return this.registerUser(event)
+      }
+    })
+    .then(() => log.debug('User auth handled'))
+    .catch(error => log.error(error))
   }
 
-  handleUserReg(event) {
+  /**
+   * registerUser
+   *
+   * @description
+   * Register new user
+   *
+   * @param  {Object} data
+   * @return {Promise}
+   */
+  registerUser (data) {
+    let { client, log, meta: { index, type } } = UserManager
+    let { userinfo, tokens } = data
+    let { sub: id, name, picture, email } = userinfo
 
-  }
-
-  registerUser(data, socketid) {
-    let { socketManager } = this.system
-    let { Status } = SocketEventEnum
-    let socket = socketManager.getSocket(socketid)
-
-    if (this.state[data.username]) {
-      socket.emit(Status.Fail, 'Username already exists')
-      return
-    } else {
-      Events.USER_REGISTERED(Object.assign({}, {
-        data: {
-          firstname: data[firstname],
-          lastname: data[lastname],
-          email: data[email],
-          userSettings: {},
-          players: {},
-          achievements: {}
-        },
-        type: 'USER_REGISTERED'
-      }))
+    let body = {
+      tokens,
+      userinfo,
+      updated_at: moment().valueOf(),
+      name,
+      picture,
+      email,
+      id,
+      achievements: [],
+      mailbox: []
     }
-    // TODO: fire event and reduce
+
+    log.debug({ id }, 'Registering new user')
+    return this.store({ id, body })
+  }
+
+  /**
+   * updateUser
+   *
+   * @description
+   * Process user login
+   *
+   * @param {data}
+   *
+   * @return {Promise}
+   */
+  updateUser (data) {
+    let { log } = UserManager
+    let { userinfo, tokens } = data
+    let { sub: id } = userinfo
+
+    let doc = {
+      tokens,
+      userinfo,
+      updated_at: moment().valueOf()
+    }
+
+    log.debug({ id }, 'Updating existing user')
+    return this.update({ id, doc })
+  }
+
+  /**
+   * getUser
+   *
+   * @description
+   * Get a user by the user id
+   *
+   * @param  {String} id
+   * @return {User}
+   */
+  getUser (id) {
+    return this.get({ id })
+      .then(response => {
+        return User.fromResponse(this, response)
+      })
+      .catch(error => Promise.reject(error))
   }
 }
 
