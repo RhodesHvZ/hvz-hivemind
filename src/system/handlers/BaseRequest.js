ebedd4b51ae55a96be208cb8e13ff2c4be42f68f
Greg Linklater <greglink49@gmail.com>
2017-05-26 16:03:00 +0200

    feat(BaseRequest): forbidden error and cleanup

--- src/system/handlers/BaseRequest.js
+++ src/system/handlers/BaseRequest.js
@@ -55,21 +55,32 @@ class BaseRequest {
     let { request: { type: request_type }, error, socket } = this
 
     if (err || error) {
-      return socket.emit('message', { type: 'FAILURE', request_type, error: error || err || 'Internal Server Error' })
+      error = error || err || 'Internal Server Error'
+      log.error({ error }, 'Internal Server Error')
+      socket.emit('message', { type: 'FAILURE', request_type, error })
     }
 
     return this
+  }
+
+  forbiddenError (err) {
+    let { request: { type: request_type }, error, socket } = this
+    log.warn(request, 'Forbidden request')
+    socket.emit('message', { type: 'FAILURE', request_type, error: error || err || 'Forbidden' })
+    return Promise.reject()
   }
 
   unauthorizedError (err) {
     let { request: { type: request_type }, error, socket } = this
     log.warn(request, 'Unauthorized request')
     socket.emit('message', { type: 'FAILURE', request_type, error: error || err || 'Unauthorized' })
+    return Promise.reject()
   }
 
   invalidRequest (err) {
     let { request: { type: request_type }, error, socket } = this
     socket.emit('message', { type: 'FAILURE', request_type, error: error || err || 'Invalid Request' })
+    return Promise.reject()
   }
 
   heartbeat (progress) {
@@ -78,9 +89,13 @@ class BaseRequest {
     socket.emit('message', { type: 'HEARTBEAT', request_type, timestamp: moment(), progress })
   }
 
-  success (response) {
-    let { request, socket } = this
+  success (instance) {
+    let { request, socket, response } = instance
     let { type: request_type } = request
+
+    if (!response) {
+      return Promise.reject('Response not present on handler')
+    }
 
     log.debug({ response }, `Request end: success`)
 
@@ -91,6 +106,8 @@ class BaseRequest {
       request_type,
       data: response
     })
+
+    return instance
   }
 }
 
