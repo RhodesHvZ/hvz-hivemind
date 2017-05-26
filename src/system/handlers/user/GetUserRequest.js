a9884b2a63917d05311047021cdcd824a3839dae
Greg Linklater <greglink49@gmail.com>
2017-05-26 16:03:13 +0200

    feat(GetUserRequest): query users

--- src/system/handlers/user/GetUserRequest.js
+++ src/system/handlers/user/GetUserRequest.js
@@ -22,29 +22,67 @@ class GetUserRequest extends BaseRequest {
 
     return Promise.resolve(instance)
       .then(instance.authenticated)
-      .then(instance.lookup)
-      .then(instance.respond)
+      .then(instance.dispatch)
+      .then(instance.success)
       .catch(error => instance.internalServerError(error))
   }
 
-  lookup (instance) {
-    let { request, system, constructor: { log } } = instance
-    let { data: { id } } = request
+  dispatch (instance) {
+    let { request: { data: { id, query } } } = instance
+
+    if (!id && !query) {
+      return instance.invalidRequest('id or query must be present')
+    }
+
     instance.heartbeat(10)
 
-    return system.userManager.getUser(id)
-      .then(user => {
-        instance.response = user
-        instance.heartbeat(75)
+    if (query) {
+      return instance.search(instance)
+    } else {
+      return instance.lookup(instance)
+    }
+  }
+
+  search (instance) {
+    let { request, system } = instance
+    let { data: { query } } = request
+
+    return system.userManager.searchUser({
+      bool: {
+        should: [
+          { regexp: { name: `.*${query}.*` } },
+          { regexp: { email: `.*${query}.*` } }
+        ]
+      }
+    }).then(users => {
+        instance.response = users
+        instance.heartbeat(80)
       })
       .then(() => instance)
       .catch(error => Promise.reject(error))
   }
 
-  respond (instance) {
-    let { response } = instance
-    instance.success(response)
-    return instance
+  fullAuthorization () {
+    let { request, socket } = this
+    let { handshake: { session: { sub } } } = socket
+    let { data: { id } } = request
+
+    this.heartbeat(30)
+
+    return id === sub
+  }
+
+  lookup (instance) {
+    let { request, system } = instance
+    let { data: { id } } = request
+
+    return system.userManager.getUser(id, instance.fullAuthorization())
+      .then(user => {
+        instance.response = user
+        instance.heartbeat(80)
+      })
+      .then(() => instance)
+      .catch(error => Promise.reject(error))
   }
 }
 
