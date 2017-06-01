b0fe50d474ffe886819de458b665360b6075be26
Greg Linklater <greglink49@gmail.com>
2017-06-01 15:53:08 +0200

    refactor(TicketManager): conform to new manager pattern

---
+++ src/system/managers/ticket/Ticket.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+
+/**
+ * Module Dependencies
+ * @ignore
+ */
+const Type = require('../../common/Type')
+
+/**
+ * Ticket
+ * @class
+ */
+class Ticket extends Type {
+
+  static get typeName () {
+    return 'ticket'
+  }
+
+}
+
+/**
+ * Export
+ * @ignore
+ */
+module.exports = Ticket
