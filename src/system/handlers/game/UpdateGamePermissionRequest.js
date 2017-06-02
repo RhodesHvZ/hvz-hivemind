17b9e8c6b96825c62e020cc9a4928a3cde95f55f
Greg Linklater <greglink49@gmail.com>
2017-06-02 16:35:46 +0200

    feat(UpdateGamePermissionRequest): change game user permissions

---
+++ src/system/handlers/game/UpdateGamePermissionRequest.js
@@ -0,0 +1,96 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+
+/**
+ * Module Dependencies
+ * @ignore
+ */
+const BaseRequest = require('../BaseRequest')
+const GameAdminRankEnum = require('../../managers/game/GameAdminRankEnum')
+
+/**
+ * UpdateGamePermissionRequest
+ * @class
+ */
+class UpdateGamePermissionRequest extends BaseRequest {
+
+  static handle (request, socket, system) {
+    let instance = new UpdateGamePermissionRequest(request, socket, system)
+
+    return Promise.resolve(instance)
+      .then(instance.authenticated)
+      .then(instance.getGame)
+      .then(instance.authorization)
+      .then(instance.userExists)
+      .then(instance.update)
+      .then(instance.success)
+      .catch(error => instance.internalServerError(error))
+  }
+
+  getGame (instance) {
+    let { request: { data }, system } = instance
+    let { gameManager } = system
+
+    if (!data) {
+      return instance.invalidRequest('data is required')
+    }
+
+    let { game_id, user_id, rank, revoke } = data
+
+    if (!game_id) {
+      return instance.invalidRequest('game_id is required')
+    }
+
+    if (!user_id) {
+      return instance.invalidRequest('user_id is required')
+    }
+
+    if (!rank && !revoke) {
+      return instance.invalidRequest('either rank or revoke is required')
+    }
+
+    return gameManager.getGame(game_id, true).then(game => {
+      instance.game = game
+      return instance
+    }).catch(error => Promise.reject(error))
+  }
+
+  authorization (instance) {
+    let { log } = UpdateGamePermissionRequest
+    let { socket, game: { admins } } = instance
+    let { handshake: { session } } = socket
+    let { sub: id } = session
+
+    let [admin] = admins.filter(admin => admin.id === id)
+
+    if (!admin || admin.rank > GameAdminRankEnum.SUPER) {
+      return instance.unauthorizedError('Insufficient privilege')
+    }
+
+    return instance
+  }
+
+  userExists (instance) {
+    return instance
+  }
+
+  update (instance) {
+    let { log } = UpdateGamePermissionRequest
+    let { request: { data }, game } = instance
+
+    return game.updateAdmin(data).then(() => {
+      instance.response = game
+      return instance
+    }).catch(error => Promise.reject(error))
+  }
+}
+
+/**
+ * Export
+ * @ignore
+ */
+module.exports = UpdateGamePermissionRequest
