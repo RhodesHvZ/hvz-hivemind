a4866c17b69e85c7892c07bb7cf75393be6ad4b8
Greg Linklater <greglink49@gmail.com>
2017-06-02 13:33:21 +0200

    feat(Game): game update in place

--- src/system/managers/game/Game.js
+++ src/system/managers/game/Game.js
@@ -4,6 +4,8 @@
  * Dependencies
  * @ignore
  */
+const moment = require('moment')
+const _ = require('lodash')
 
 /**
  * Module Dependencies
@@ -32,6 +34,47 @@ class Game extends Type {
 
   static get typeName () {
     return 'game'
+  }
+
+  update (data) {
+    if (!data) {
+      return Promise.reject('data is required')
+    }
+
+    let { manager, id } = this
+    let { description, background_image, registration_date, start_date, end_date, rules } = data
+    let { log } = Game
+
+    if (registration_date) {
+      let normalizedRegDate = moment(registration_date)
+      registration_date = normalizedRegDate.isValid() ? normalizedRegDate.valueOf() : undefined
+    }
+
+    if (start_date) {
+      let normalizedStartDate = moment(start_date)
+      start_date = normalizedStartDate.isValid() ? normalizedStartDate.valueOf() : undefined
+    }
+
+    if (end_date) {
+      let normalizedEndDate = moment(end_date)
+      end_date = normalizedEndDate.isValid() ? normalizedEndDate.valueOf() : undefined
+    }
+
+    let doc = _.omitBy({
+      description,
+      background_image,
+      registration_date,
+      start_date,
+      end_date,
+      rules
+    }, value => value === undefined)
+
+    return manager.update({ id, doc })
+      .then(result => {
+        Object.assign(this, doc)
+        return result
+      })
+      .catch(error => Promise.reject(error))
   }
 
 }
