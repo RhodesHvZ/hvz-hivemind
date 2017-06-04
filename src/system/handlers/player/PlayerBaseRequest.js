43b41d72a0f0872299625c1c422aa30c3abd53bd
Greg Linklater <greglink49@gmail.com>
2017-06-04 23:53:18 +0200

    feat(PlayerBaseRequest): misc. helper methods to fetch player objects

--- src/system/handlers/player/PlayerBaseRequest.js
+++ src/system/handlers/player/PlayerBaseRequest.js
@@ -15,7 +15,34 @@ const GameBaseRequest = require('../game/GameBaseRequest')
  * PlayerBaseRequest
  * @class
  */
-class PlayerBaseRequest extends GameBaseRequest {}
+class PlayerBaseRequest extends GameBaseRequest {
+
+  getPlayer (instance) {
+    let { request: { data: { player_id } }, game: { playerManager } } = instance
+
+    return playerManager.get({ id: player_id, safe: true }).then(player => {
+      instance.player = player
+      return instance
+    }).catch(error => Promise.reject(error))
+  }
+
+  getPlayerByUser(instance) {
+    let { socket, game: { playerManager, id: game_id } } = instance
+    let { handshake: { session: { sub: user_id } } } = socket
+
+    return playerManager.getByUser(user_id)
+      .then(players => {
+        if (players.length === 1) {
+          instance.player = players[0]
+          return instance
+        } else {
+          return instance.invalidRequest(`User ${user_id} doesn't have a player for game ${game_id}`)
+        }
+      })
+      .catch(error => Promise.reject(error))
+  }
+
+}
 
 /**
  * Export
