28bc4be8856b58f6589b783893f20668213b720b
Greg Linklater <greglink49@gmail.com>
2017-06-06 16:33:40 +0200

    wip(TicketBaseRequest): abstract base handler class for ticket requests

---
+++ src/system/handlers/ticket/TicketBaseRequest.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+
+/**
+ * Module Dependencies
+ * @ignore
+ */
+const BaseRequest = require('../BaseRequest')
+
+/**
+ * TicketBaseRequest
+ * @class
+ */
+class TicketBaseRequest extends BaseRequest {
+}
+
+/**
+ * Export
+ * @ignore
+ */
+module.exports = TicketBaseRequest
