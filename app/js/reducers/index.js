5f3c4f4368cc792eeecaa1254b05643019d91d3b
Greg Linklater <greglink49@gmail.com>
2017-06-07 23:12:20 +0200

    feat(reducers): add Session, Heartbeat and User reducers

--- app/js/reducers/index.js
+++ app/js/reducers/index.js
@@ -1,9 +1,33 @@
 'use strict'
 
+/**
+ * UI Reducers
+ * @ignore
+ */
 import ToggleSidebarReducer from './ToggleSidebarReducer'
 import ToggleUserPopdownReducer from './ToggleUserPopdownReducer'
 
+/**
+ * Socket Response Reducers
+ * @ignore
+ */
+import HeartbeatRequestReducer from './socket/HeartbeatRequestReducer'
+import SessionRequestReducer from './socket/SessionRequestReducer'
+import UserRequestsReducer from './socket/UserRequestsReducer'
+
+/**
+ * Export
+ * @ignore
+ */
 export default {
+  // SYSTEM / DEBUG
+  heartbeat: (state, action) => HeartbeatRequestReducer.reduce(state, action),
+  session: (state, action) => SessionRequestReducer.reduce(state, action),
+
+  // USER
+  users: (state, action) => UserRequestsReducer.reduce(state, action),
+
+  // UI
   sidebarOpen: ToggleSidebarReducer,
   userPopdownData: ToggleUserPopdownReducer
 }
