68f0a6c83e782c771cdbc45ce7a52289f77ebe23
Greg Linklater <greglink49@gmail.com>
2017-06-13 13:44:25 +0200

    fix(UserRequestsReducer): reduce users

--- app/js/reducers/socket/UserRequestsReducer.js
+++ app/js/reducers/socket/UserRequestsReducer.js
@@ -22,30 +22,36 @@ class UserRequestsReducer {
     let { data, type } = action
 
     if (this.typeNames.includes(type)) {
-      return this.updateUser(state, data)
+      return this.updateUsers(state, data)
     }
 
     return state
   }
 
-  static updateUser (state, data) {
-    if (Array.isArray(data)) {
-      if (data.length === 0) {
-        return state
-      }
+  static updateUsers (state, data) {
+    if (Array.isArray(data) && data.length > 0) {
+      let updated = data.reduce((acc, user) => {
+        let { id } = user
+        acc[id] = Object.assign(this.updateUser(state[id], user))
+        return acc
+      }, {})
 
-      return data.reduce((state, curr) => {
-        let { id } = curr
-        state[id] = curr
-        return state
-      }, Object.assign({}, state))
+      return Object.assign({}, state, updated)
 
-    } else {
+    } else if (typeof data === 'object') {
       let { id } = data
-      return Object.assign({}, state, { [id]: data })
+      return Object.assign({}, state, { [id]: this.updateUser(state[id], data) })
     }
 
     return state
+  }
+
+  static updateUser (existing, data) {
+    if (data) {
+      return existing ? Object.assign({}, existing, data) : data
+    }
+
+    return existing || {}
   }
 }
 
