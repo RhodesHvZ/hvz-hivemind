b4f3fd8240072f0a484d6de4635a3fbd276ce59b
Greg Linklater <greglink49@gmail.com>
2017-06-24 16:57:19 +0200

    feat(GetAllGamesAction)

---
+++ app/js/actions/socket/GetAllGamesAction.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+import SocketManager from '../../SocketManager'
+
+/**
+ * GetAllGamesAction
+ */
+export default function GetAllGamesAction (query) {
+  SocketManager.send({ type: 'GAME_GET' })
+}
