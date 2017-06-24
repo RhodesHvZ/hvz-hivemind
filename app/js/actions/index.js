a501613746e35aa281372f9f1c2e11d4e32e47a8
Greg Linklater <greglink49@gmail.com>
2017-06-24 16:59:55 +0200

    feat(actions): game actions

--- app/js/actions/index.js
+++ app/js/actions/index.js
@@ -6,10 +6,22 @@
  */
 import HeartbeatAction from './socket/HeartbeatAction'
 import GetSessionAction from './socket/GetSessionAction'
+
+// USER
 import GetUserAction from './socket/GetUserAction'
 import SearchUserAction from './socket/SearchUserAction'
+
+// MAIL
 import GetMailAction from './socket/GetMailAction'
 import PrivateMessageAction from './socket/PrivateMessageAction'
+
+// GAME
+import NewGameAction from './socket/NewGameAction'
+import GetGameAction from './socket/GetGameAction'
+import GetAllGamesAction from './socket/GetAllGamesAction'
+import UpdateGameAdminPermissionAction from './socket/UpdateGameAdminPermissionAction'
+import RevokeGameAdminPermissionAction from './socket/RevokeGameAdminPermissionAction'
+import TransferGameOwnershipAction from './socket/TransferGameOwnershipAction'
 
 /**
  * Export
@@ -18,8 +30,20 @@ import PrivateMessageAction from './socket/PrivateMessageAction'
 export {
   HeartbeatAction,
   GetSessionAction,
+
+  // USER
   GetUserAction,
   SearchUserAction,
+
+  // MAIL
   GetMailAction,
   PrivateMessageAction,
+
+  // GAME
+  NewGameAction,
+  GetGameAction,
+  GetAllGamesAction,
+  UpdateGameAdminPermissionAction,
+  RevokeGameAdminPermissionAction,
+  TransferGameOwnershipAction,
 }
