b99948da3644ede863f70588c9e673583eb1e65c
Greg Linklater <greglink49@gmail.com>
2017-06-24 18:33:23 +0200

    feat(UpdateGameAction): and add actions to index

--- app/js/actions/index.js
+++ app/js/actions/index.js
@@ -19,9 +19,25 @@ import PrivateMessageAction from './socket/PrivateMessageAction'
 import NewGameAction from './socket/NewGameAction'
 import GetGameAction from './socket/GetGameAction'
 import GetAllGamesAction from './socket/GetAllGamesAction'
+import UpdateGameAction from './socket/UpdateGameAction'
 import UpdateGameAdminPermissionAction from './socket/UpdateGameAdminPermissionAction'
 import RevokeGameAdminPermissionAction from './socket/RevokeGameAdminPermissionAction'
 import TransferGameOwnershipAction from './socket/TransferGameOwnershipAction'
+
+// PLAYER
+import GetPlayerAction from './socket/GetPlayerAction'
+// import GetOwnPlayersAction from './socket/GetOwnPlayersAction'
+import UpdatePlayerAction from './socket/UpdatePlayerAction'
+import SearchPlayerAction from './socket/SearchPlayerAction'
+import RegisterPlayerAction from './socket/RegisterPlayerAction'
+import ActivatePlayerAction from './socket/ActivatePlayerAction'
+import DeactivatePlayerAction from './socket/DeactivatePlayerAction'
+import KillPlayerAction from './socket/KillPlayerAction'
+import RevivePlayerAction from './socket/RevivePlayerAction'
+import SuspendPlayerAction from './socket/SuspendPlayerAction'
+import AddPlayerSuperstateAction from './socket/AddPlayerSuperstateAction'
+import RemovePlayerSuperstateAction from './socket/RemovePlayerSuperstateAction'
+import TagPlayerAction from './socket/TagPlayerAction'
 
 /**
  * Export
@@ -43,7 +59,23 @@ export {
   NewGameAction,
   GetGameAction,
   GetAllGamesAction,
+  UpdateGameAction,
   UpdateGameAdminPermissionAction,
   RevokeGameAdminPermissionAction,
   TransferGameOwnershipAction,
+
+  // PLAYER
+  GetPlayerAction,
+  // GetOwnPlayersAction,
+  UpdatePlayerAction,
+  SearchPlayerAction,
+  RegisterPlayerAction,
+  ActivatePlayerAction,
+  DeactivatePlayerAction,
+  KillPlayerAction,
+  RevivePlayerAction,
+  SuspendPlayerAction,
+  AddPlayerSuperstateAction,
+  RemovePlayerSuperstateAction,
+  TagPlayerAction,
 }
---
+++ app/js/actions/socket/UpdateGameAction.js
@@ -0,0 +1,42 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+import SocketManager from '../../SocketManager'
+
+/**
+ * UpdateGameAction
+ *
+ * @param  {Object} options
+ * @param  {String} options.game_id
+ * @param  {String} [options.name]
+ * @param  {String} [options.registration_date]
+ * @param  {String} [options.start_date]
+ * @param  {String} [options.end_date]
+ * @param  {String} [options.description]
+ * @param  {URL} [options.rules]
+ * @param  {URL} [options.background_image]
+ */
+export default function UpdateGameAction (options) {
+  let { game_id, name, registration_date, start_date, end_date, description, rules, background_image } = options
+
+  if (!game_id) {
+    throw new Error(`Missing required argument 'game_id'`)
+  }
+
+  SocketManager.send({
+    type: 'GAME_UPDATE',
+    data: {
+      game_id,
+      name,
+      registration_date,
+      start_date,
+      end_date,
+      description,
+      rules,
+      background_image,
+    }
+  })
+}
