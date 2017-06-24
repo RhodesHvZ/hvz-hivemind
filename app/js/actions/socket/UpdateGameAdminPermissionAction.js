ed4c220f2bc2449e9b6afc1b07e1c432f2a429d9
Greg Linklater <greglink49@gmail.com>
2017-06-24 16:57:58 +0200

    feat(UpdateGameAdminPermissionAction)

---
+++ app/js/actions/socket/UpdateGameAdminPermissionAction.js
@@ -0,0 +1,37 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+import SocketManager from '../../SocketManager'
+
+/**
+ * UpdateGameAdminPermissionAction
+ *
+ * @param  {String} game_id
+ * @param  {String} user_id
+ * @param  {Number} rank
+ */
+export default function UpdateGameAdminPermissionAction (game_id, user_id, rank) {
+  if (!game_id) {
+    throw new Error(`Missing required argument 'game_id'`)
+  }
+
+  if (!user_id) {
+    throw new Error(`Missing required argument 'user_id'`)
+  }
+
+  if (!rank) {
+    throw new Error(`Missing required argument 'rank'`)
+  }
+
+  SocketManager.send({
+    type: 'GAME_PERMISSION_UPDATE',
+    data: {
+      game_id,
+      user_id,
+      rank
+    }
+  })
+}
