85a8cd105d63032eb923587fd49b0eec62184764
Greg Linklater <greglink49@gmail.com>
2017-06-24 16:57:05 +0200

    feat(GetGameAction)

---
+++ app/js/actions/socket/GetGameAction.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+import SocketManager from '../../SocketManager'
+
+/**
+ * GetGameAction
+ *
+ * @param {String} game_id
+ */
+export default function GetGameAction (game_id) {
+  if (!game_id) {
+    throw new Error(`Missing required argument 'game_id'`)
+  }
+
+  SocketManager.send({
+    type: 'GAME_GET',
+    data: {
+      id: game_id
+    }
+  })
+}
