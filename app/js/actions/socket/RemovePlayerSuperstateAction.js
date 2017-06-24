244a7889cd90892e7ba45c16d360239702083b1a
Greg Linklater <greglink49@gmail.com>
2017-06-24 18:31:50 +0200

    feat(RemovePlayerSuperstateAction)

---
+++ app/js/actions/socket/RemovePlayerSuperstateAction.js
@@ -0,0 +1,38 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+import SocketManager from '../../SocketManager'
+
+/**
+ * RemovePlayerSuperstateAction
+ *
+ * @param  {String} game_id
+ * @param  {String} player_id
+ * @param  {String} superstate
+ */
+export default function RemovePlayerSuperstateAction (game_id, player_id, superstate) {
+  if (!game_id) {
+    throw new Error(`Missing required argument 'game_id'`)
+  }
+
+  if (!player_id) {
+    throw new Error(`Missing required argument 'player_id'`)
+  }
+
+  if (!superstate) {
+    throw new Error(`Missing required argument 'superstate'`)
+  }
+
+  SocketManager.send({
+    type: 'PLAYER_SUPERSTATE',
+    data: {
+      game_id,
+      player_id,
+      superstate,
+      remove: true,
+    }
+  })
+}
